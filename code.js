9a7e59884547139f77952fe98a6ea06cd9887bad
User <mdshishov@mail.ru>
2025-04-10 03:42:53 +0300

    Начало обновления страницы с проектами

--- code.js
+++ code.js
@@ -1,1 +1,33 @@
-setTimeout(() => this, 0);
+async function app() {
+  const posts = [];
+  try {
+    posts.push(...await fetch('./posts.json').then((result) => result.json()));
+  } catch (error) {
+    showErrorMessage();
+    return;
+  }
+
+  console.log(posts);
+
+  const uiState = {
+    positions: [],
+  }
+  
+  const state = { posts, uiState };
+  
+  function generateStartPosition() {}
+  
+  function renderStartState() {}
+  
+  function createPostEl(postData) {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+  
+    const header = document.createElement('div');
+    header.classList.add('post__header');
+    header.textContent = postData.header;
+  }
+  function showErrorMessage() {}
+}
+
+document.addEventListener('DOMContentLoaded', app);
